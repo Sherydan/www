5be9e03c56b6d9546906cc066eff78f9042ecf3d
radge91 <edgardo.ig.avila@gmail.com>
2016-07-18 22:23:21 -0400

    bootstra
    as

--- wp-content/themes/radge/index.php
+++ wp-content/themes/radge/index.php
@@ -3,22 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php bloginfo('description');?></title>
-<!-- Latest compiled and minified CSS -->
-
-
-
-<link rel="stylesheet" href="<?php bloginfo(stylesheet_url)?>">
-
+<!-- hojas de estilo y boostrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php bloginfo(stylesheet_url)?>">
 </head>
-<body>
-<header >
-<nav class="navbar navbar-fixed-top navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">Mi portafolio</a>
-</nav>
-</header>
 
-<p class="textos">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam amet sapiente distinctio, temporibus tenetur sint, blanditiis non ipsam minima maiores, ipsum illum molestias, deleniti rerum qui! Autem, sed quasi culpa.
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ad labore aut numquam fugit. Perspiciatis illo illum, optio quisquam eius quae facilis natus placeat temporibus ipsum ea eos vitae perferendis!</p>
+<body>
+
+<header>
+<nav class="navbar navbar-dark bg-inverse">
+  <!-- Navbar content -->
+</nav>	
+</header>
+<section class="main container"></section>
+<footer></footer>
+
+
+
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
