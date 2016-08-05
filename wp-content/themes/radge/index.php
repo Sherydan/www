639f09e0658e72ffc89bd0da22d187cf36b8771a
radge91 <edgardo.ig.avila@gmail.com>
2016-08-05 14:33:11 -0400

    lipiezaarduino

--- wp-content/themes/radge/index.php
+++ wp-content/themes/radge/index.php
@@ -45,28 +45,48 @@
       </div><!-- /.container -->
     </nav><!-- /.navbar -->
 
-   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+ 
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
   <ol class="carousel-indicators">
     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
   </ol>
-  <div class="carousel-inner" role="listbox">
-    <div class="carousel-item active">
-      <img src="http://localhost/imagenes/cd.jpg" alt="First slide">
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://srowlen.com/wp-content/uploads/sunrise-11200x315.png" alt="...">
+      <div class="carousel-caption" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="http://localhost/imagenes/click.jpg" alt="Second slide">
+    <div class="item">
+      <img src="http://srowlen.com/wp-content/uploads/best-landscape-photography25-magnificent-natural-landscape-photography-examples-for-your-ftevaupm1200x315.jpg" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://srowlen.com/wp-content/uploads/sunrise-11200x315.png" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
     </div>
   </div>
+ 
+  <!-- Controls -->
   <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="icon-prev" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+    <span class="glyphicon glyphicon-chevron-left"></span>
   </a>
   <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="icon-next" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+    <span class="glyphicon glyphicon-chevron-right"></span>
   </a>
-</div>
+</div> <!-- Carousel -->
+
+
 
     <div class="container">
       <!-- Example row of columns -->
